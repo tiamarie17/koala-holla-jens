fafe72ab6584c44a87ef6ea43a3a25f84f179f51
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-06 13:32:51 -0500

    render koalas

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,7 +6,6 @@ $( document ).ready( function(){
   setupClickListeners()
   // load existing koalas on page load
   getKoalas();
-
 }); // end doc ready
 
 function setupClickListeners() {
@@ -30,11 +29,32 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  //add in render(response) in get .then()
 } // end getKoalas
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
  
+}
+
+
+function render(array){
+  console.log('in render fn');
+$('#viewKoalas').empty();;
+
+for(let koala of array){
+  $('#viewKoalas').append(`
+    <tr>
+      <td>${koala.name}</td>
+      <td>${koala.age}</td>
+      <td>${koala.gender}</td>
+      <td>${koala.ready_to_transfer}</td>
+      <td>
+        <button id="transferBn" data-id="${koala.id}">Ready to Transfer</button>
+      </td>
+      <td>${koala.notes}</td>
+    </tr>
+  `)
+};
 }
