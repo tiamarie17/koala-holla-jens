1065e28ab7578b4dcc658135c3a20cda775783e6
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-07 09:54:55 -0500

    update database to boolean value

--- database.sql
+++ database.sql
@@ -1,20 +1,21 @@
+DROP TABLE IF EXISTS "koallas";
+
+
 CREATE TABLE "koallas" (
 "id" serial primary key,
 "name" varchar not null,
 "gender" varchar(1) not null,
 "age" integer,
-"ready_to_transfer" varchar(1) not null,
+"ready_to_transfer" BOOLEAN DEFAULT FALSE,
 "notes" varchar not null
 );
 
 
 INSERT INTO "koallas"("name", "gender", "age", "ready_to_transfer", "notes")
 VALUES
-('SCOTTY', 'M' , '4' , 'Y' , 'Born in Guatemala'),
-('Jean', 'F', '5' , 'Y', 'Allergic to lots of lava'),
-('Orono', 'F', '7' , 'N', 'Loves listening to Paula (Abdul)'),
-('Logan', 'M', '15' , 'N' , 'Loves the sauna'),
-('Charlie' , 'M', '9' , 'Y' , 'Favorite band is Nirvana'),
-('Betsy', 'F' , '4' , 'Y' , 'Has a pet iguana');
-
-
+('SCOTTY', 'M' , '4' , 'True' , 'Born in Guatemala'),
+('Jean', 'F', '5' , 'True', 'Allergic to lots of lava'),
+('Orono', 'F', '7' , 'False', 'Loves listening to Paula (Abdul)'),
+('Logan', 'M', '15' , 'False' , 'Loves the sauna'),
+('Charlie' , 'M', '9' , 'True' , 'Favorite band is Nirvana'),
+('Betsy', 'F' , '4' , 'True' , 'Has a pet iguana');
